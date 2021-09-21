85290db5a58d634bfe7dcbe62f6b3ac20b035c74
Alexandra-the-Great <70353168+Alexandra-the-Great@users.noreply.github.com>
2021-09-20 23:51:42 -0500

    Adding Lab4a

---
+++ Lab4a_Act1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#By submitting this assignment, I agree to the following:
+#    "Aggies do not lie, cehat, or steal, or tolerate those who do."
+#    "I have not given or received any unauthorized aid on this assignment."
+#    
+#Name:       Alexandra Mitchell
+#Section:    568
+#Assignment: Lab 4a_Act_Act1
+#Date:       20 9 2021
+
+print("The entire bee movie script")
+
+
